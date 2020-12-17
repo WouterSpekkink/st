89bfadb6bed31aaac2c33c8b0d0ab4b5d682d806
WouterSpekkink <wouterspekkink@gmail.com>
2020-12-17 20:58:08 +0100

    fixed mistakes I made in mouse scroll patching.

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 7;
 
 /*
@@ -96,24 +96,24 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#2d2d2d",
-	"#f2777a",
-	"#99cc99",
-	"#ffcc66",
-	"#3366cc",
-	"#cc99cc",
-	"#66cccc",
-	"#d3d0c8",
+	"#1d1f21",
+	"#cc6666",
+	"#b5bd68",
+	"#f0c674",
+	"#81a2be",
+	"#b294bb",
+	"#8abeb7",
+	"#c5c8c6",
 
 	/* 8 bright colors */
-	"#747369",
-	"#f2777a",
-	"#99cc99",
-	"#ffcc66",
-	"#3366cc",
-	"#cc99cc",
-	"#66cccc",
-	"#f2f0ec",
+	"#666666",
+	"#d54e53",
+	"#b9ca4a",
+	"#e7c547",
+	"#7aa6da",
+	"#c397d8",
+	"#70c0b1",
+	"#eaeaea",
 
 	[255] = 0,
 
@@ -187,8 +187,10 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ ShiftMask,		Button4, kscrollup,	 {.i = 1}, 	0, /* !alt */ -1 },
-	{ ShiftMask, 		Button5, kscrolldown,	 {.i = 1}, 	0, /* !alt */ -1 },	
+	{ ShiftMask,		Button4, kscrollup, 	 {.i = 1} },
+	{ ShiftMask, 		Button5, kscrolldown,    {.i = 1} },
+	{ XK_ANY_MOD,		Button4, kscrollup,	 {.i = 1}, 	0, /* !alt */ -1 },
+	{ XK_ANY_MOD, 		Button5, kscrolldown,	 {.i = 1}, 	0, /* !alt */ -1 },	
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
